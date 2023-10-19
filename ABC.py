53b05efed517f140d8bcffd4a933faf0a784de3c
varshuachar10 <144010279+varshuachar10@users.noreply.github.com>
2023-10-19 23:50:25 +0530

    Add files via upload

---
+++ ABC.py
@@ -0,0 +1,14 @@
+def encrypt(text,s):
+    result=""
+    for i in range(len(text)):
+        char=text[i]
+        if(char.isupper()):
+            result+=chr((ord(char)+s-65)%26+65)
+        else:
+            result+=chr((ord(char)+s-97)%26+97)
+    return result
+text="ATTACKTONCE"
+s=4
+print("Text:"+text)
+print("Shift:"+str(s))
+print("Ciper:"+encrypt(text,s)) 
